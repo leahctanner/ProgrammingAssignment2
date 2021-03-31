834e5382f7e8b02c28df94a5be5bf225bbe688b9
Leah Tanner <leah.tanner1@gmail.com>
2021-03-30 21:19:59 -0400

    Creating both makecahce and solvecache functions to calculate the inverse of a simple 4x4 matrix called "Leahs Matrix"

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_mx <- NULL
+        set <- function(y) {
+                x <<- y
+                inv_mx <<- NULL
+        }
+        get <- function() {x}
+        set_inverse <- function(inverse) {inv_mx <<- inverse}
+        get_inverse <- function() {inv_mx}
+        list(set=set, get=get, set_inverse=set_inverse, get_inverse=get_inverse)
 }
 
 
 ## Write a short comment describing this function
+## This function is used to test whether the matric has been cached and is able to produce an inverse. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv_mx <- x$get_inverse()
+        if(!is.null(inv_mx)){
+                message("you can't do that yet")
+                return(inv_mx)
+        }
+        leahsmatrix <- x$get()
+        inv_mx <- solve(leahsmatrix, ...)
+        x$set_inverse(inv_mx)
+        inv_mx
 }
